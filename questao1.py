bbd0dd5991d5e41b75a698896576c1738012a738
Caio <121897609+Bunnyzzx@users.noreply.github.com>
2026-08-07 22:34:33 -0300

    Delete questao1.py

--- questao1.py
+++
@@ -1,9 +0,0 @@
-nome = input("Digite seu nome: ")
-
-print(nome)
-print(nome.upper())
-print(len(nome.replace(" ","")))
-partes = nome.split()
-partes[-1] = "Do Inatel"
-new_nome = " ".join(partes)
-print(new_nome)
